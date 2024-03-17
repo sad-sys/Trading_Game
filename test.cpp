23528b9087681980abdc436fc8a49a4af34c7c4e
sad-sys <khawajasadiq25@gmail.com>
2024-03-17 14:17:32 +0000

    Added while loop and various options for user + profile vector

--- test.cpp
+++ test.cpp
@@ -4,63 +4,99 @@
 #include <string>
 #include <vector>
 
+bool contains(const std::string arr[], int size, const std::string& element)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (arr[i] == element)
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
-int checkFile(const std::string&fileName)
+std::string command()
+{
+    std:: string commandAction;
+    std::cout << "Type a command, buy, sell, check, next and profile  \n" << std::endl;
+    std::cin >> commandAction;
+    return commandAction;
+}
+
+bool checkFile(const std::string&fileName)
 {
     std::ifstream file(fileName);
     if (!file.is_open()) 
     {
         std::cerr << "Error opening file" << std::endl;
-        return 1;
+        return false;
     }
     else
     {
-        return 0;
+        return true;
     }
 }
 
 int main() 
 {
     float money = 100.00;
-    std::cout << "Day is 2013-02-08 \nYour Bank Account is at "<< money << "\n";
+    std::cout << "Day is 2013-02-08 \nYour Bank Account is at $"<< money << "\n";
 
+    std::vector<std::string> userProfile;
+    std::string stockChoices[1];
+    stockChoices[0] = "A";
+
+    std::string commandA;
+
+    while (true)
+    {
+        commandA  = command();
+        if (commandA == "check")
+        {
+            std::cout <<"$"<<money<<"\n";
+        }
+        else if (commandA == "next")
+        {
+            std::cout << "next!\n";
+        }
+        else if (commandA == "buy")
+        {
+            std::string stockChoice;
+            std::cout << "Which stock would you like to buy?\n";
+            std::cin >> stockChoice;
+            if (contains(stockChoices, 1, stockChoice))
+            {
+                money = money - 10;
+                std::cout << money << "\n";
+            }
+        }
+        else if (commandA == "sell")
+        {
+            std::cout << "sell!\n";
+        }
+        else if (commandA == "profile")
+        {
+            std::cout << "profile\n";
+        }
+        else
+        {
+            std::cout << "Not an option\n";
+        }
+    }
+
+
+
+}
+
+/*
     std::string stock;
-    std::cout << "Type a Stock      ";
-    std::cin >> stock;
     std::string csvFile;
+    stock = "A";
     csvFile = ".csv";
 
     stock = stock + csvFile;
 
     int answer = checkFile(stock);
     std::cout << answer << "\n" << std::endl;
-
-    return 0;
-}
-
-
-
-/*
-    std::string line;
-    while (std::getline(file, line)) 
-    {
-        std::vector<std::string> row;
-        std::stringstream ss(line);
-
-        std::string value;
-        while (std::getline(ss, value, ',')) 
-        {
-            row.push_back(value);
-        }
-
-        // Process the row
-        for (const auto& field : row) 
-        {
-            std::cout << field << " ";
-        }
-        std::cout << std::endl;
-    }
-
-    file.close();
-
-    */
+*/
